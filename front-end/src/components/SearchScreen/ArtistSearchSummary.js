e9043346e5c382425c4e107011ec43121c9b4dcd
Marley Robinson <marlsrobo@gmail.com>
2022-04-10 17:52:46 -0400

    search results for artists prettied up, still need to configure for tracks and users and details for all

---
+++ front-end/src/components/SearchScreen/ArtistSearchSummary.js
@@ -0,0 +1,51 @@
+const ArtistSearchSummary = ({artist}) => {
+
+    const imageStyle = {
+        width: "150px",
+        height: "150px",
+        objectFit: "cover",
+        borderRadius: "20px"
+    }
+
+    const formatGenres = (genres) => {
+        let result = "";
+        for (let i = 0; i < genres.length; i++) {
+            result += genres[i] + ", "
+        }
+        if (genres.length !== 0) {
+            result = result.slice(0, -2);
+        }
+        return result;
+    }
+
+    const getImage = (artist) => {
+        try {
+            return <img id="artistImg" src={artist.images[0].url} className="float-start me-3"
+                        style={imageStyle}/>
+        }
+        catch (Exception) {
+            return <img id="artistImg" src="images/blankProfile.png" className="float-start me-3"
+                        style={imageStyle}/>
+        }
+    }
+
+    return(
+        <li className="list-group-item pt-3 pb-3">
+            {getImage(artist)}
+            <div className="float-start">
+                <h5>{artist.name}</h5>
+                Popularity: {artist.popularity}
+                <br/>
+                Followers: {artist.followers.total.toLocaleString("en-US")}
+                <br/>
+                Genres: {formatGenres(artist.genres)}
+                <br/>
+                <a href={artist.external_urls.spotify} target="_blank">Listen on Spotify</a>
+            </div>
+
+        </li>
+    );
+
+};
+
+export default ArtistSearchSummary;
--- front-end/src/components/SearchScreen/index.js
+++ front-end/src/components/SearchScreen/index.js
@@ -1,16 +1,27 @@
 import NavigationSidebar from "../NavigationSidebar";
 import React, {useEffect, useRef, useState} from "react";
 import {findArtistsQuery} from "../Services/spotify-api-services";
+import ArtistSearchSummary from "./ArtistSearchSummary";
 
 const SearchScreen = () => {
     const [results, setResults] = useState([]);
     const searchRef = useRef();
+
+    let searchBy = "artist";
+
+    // let searchBy = document.getElementById("searchByDropdown");
 
     const search = async () => {
         const searchKey = searchRef.current.value;
         const response = await findArtistsQuery(searchKey);
         setResults(response);
     };
+
+    const getSelectedSearchBy = () => {
+        var selectElement = document.querySelector('#searchByDropdown');
+        var output = selectElement.value;
+        searchBy = output;
+    }
 
     return(
         <>
@@ -23,7 +34,8 @@ const SearchScreen = () => {
             <div className="col-10 col-lg-11 col-xl-10 mt-3">
                 <div className="row mt-2">
                     <div className="col-2">
-                        <select defaultValue="artist" className="form-select" style={{"height": "40px"}}>
+                        <select id="searchByDropdown" defaultValue="artist" className="form-select" style={{"height": "40px"}}
+                        onChange={getSelectedSearchBy}>
                             <option value="artist">Artist</option>
                             <option value="track">Song</option>
                             <option value="user">User</option>
@@ -43,16 +55,15 @@ const SearchScreen = () => {
                         </button>
                     </div>
                 </div>
+                <ul className="list-group mt-3">
+                    {
+                        results.map(artist => <ArtistSearchSummary artist={artist}/>)
+                    }
+
+                </ul>
             </div>
         </div>
-        <ul className="list-group">
-            {
-                results.map(artist => <li>
-                    {artist.name}
-                </li>)
-            }
 
-        </ul>
 
         </>
     );
