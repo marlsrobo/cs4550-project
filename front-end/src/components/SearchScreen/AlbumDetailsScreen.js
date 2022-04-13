a079ac42117e7798d83b829df881b6e62c3e6b15
Marley Robinson <marlsrobo@gmail.com>
2022-04-13 13:14:18 -0400

    album details done except for pulling info from own db

--- front-end/src/components/SearchScreen/AlbumDetailsScreen.js
+++ front-end/src/components/SearchScreen/AlbumDetailsScreen.js
@@ -1,4 +1,4 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 import {fetchAlbumByIdFromSpotify} from "../Services/spotify-api-services";
 import NavigationSidebar from "../NavigationSidebar";
@@ -9,43 +9,73 @@ const AlbumDetailsScreen = () => {
     const [albumDetails, setAlbumDetails] = useState({});
     const getAlbumDetails = async () => {
         let details = await fetchAlbumByIdFromSpotify(albumId);
-
-        console.log(details);
+        // console.log(details);
         return details.data;
         // setAlbumDetails(details.data);
         // console.log(details.data);
       // console.log(albumDetails);
     };
 
-    const formatArtists = (artists) => {
-        let result = "";
-        console.log(artists);
-        for (let i = 0; i < artists.length; i++) {
-            result += artists[i].name + ", "
-        }
-        if (artists.length !== 0) {
-            result = result.slice(0, -2);
-        }
-        return result;
-    }
-
     const getImage = (album) => {
         try {
-            return <img id="albumImg" src={album.images[0].url} className=" me-3" height="400px"/>
+            return <img id="albumImg" src={album.images[0].url} className="me-3" height="400px"/>
         }
         catch (Exception) {
-            return <img id="albumImg" src="images/blankProfile.png" className=" me-3"/>
+            return <img id="albumImg" src="/images/blankProfile.png" className=" me-3"/>
         }
-    }
+    };
 
     const getArtists = (album) => {
         try {
-            return <h2>{album.artists[0].name}</h2>
+            return <h3><Link to={`/artist/details/${album.artists[0].id}`}>{album.artists[0].name}</Link></h3>
         }
         catch (Exception) {
-            return <h2>hi</h2>
         }
+    };
+
+    const millisToMinutesAndSeconds = (millis) => {
+        let minutes = Math.floor(millis / 60000);
+        let seconds = ((millis % 60000) / 1000).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
     }
+
+    const formatTrack = (track) => {
+        return(
+          <li className="list-group-item">
+              <span className="float-start">
+                  {track.track_number}. {track.name} <span style={{"color": "red"}}>{track.explicit && "EXPLICIT"}</span>
+              </span>
+              <span className="float-end">
+                  {millisToMinutesAndSeconds(track.duration_ms)}
+              </span>
+          </li>
+        );
+    };
+
+    const formatTracks = (album) => {
+        try {
+            console.log(album.tracks.items);
+            return(
+                <ul className="list-group">
+
+                    {
+                        album.tracks.items.map(track => formatTrack(track))
+                    }
+                </ul>
+            );
+        } catch (Exception) {
+
+        }
+    };
+
+    const formatAlbumLinkToSpotify = (album) => {
+        try {
+            return (
+                <a href={album.external_urls.spotify} target="_blank"><img src="/images/Spotify_icon.png" className="float-end" height="100px"/></a>
+            );
+
+        } catch (Exception) { }
+    };
 
     useEffect(() => {
         getAlbumDetails().then(album => setAlbumDetails(album));
@@ -59,15 +89,22 @@ const AlbumDetailsScreen = () => {
                 <NavigationSidebar active="search"/>
             </div>
             <div className="col-10 col-lg-11 col-xl-10 mt-3">
-                <h1>{albumDetails.name}</h1>
+                <div className="float-start">
+                    {getImage(albumDetails)}
+                    <br/>
+                    <button className="mt-3 me-3 btn btn-success"><i className="far fa-thumbs-up me-1 wd-14px-font wd-gray-color"></i></button>
+                    <button className="mt-3 btn btn-danger"><i className="far fa-thumbs-down me-1 wd-14px-font wd-gray-color"></i></button>
+                    <br/>
+                    <textarea className="mt-3 form-control me-2" placeholder="Leave a review" style={{"width": "400px"}}></textarea>
+                    <button className="mt-3 btn btn-secondary">Submit</button>
+
+                </div>
+                <div>
+                    {formatAlbumLinkToSpotify(albumDetails)}
+                    <h1>{albumDetails.name}</h1>
+                </div>
                 {getArtists(albumDetails)}
-                {/*<h2>{albumDetails.images[0].url}</h2>*/}
-                {/*<h2>{formatArtists(albumDetails.artists)}</h2>*/}
-                {/*<h2>{albumDetails.artists[0].name}</h2>*/}
-                <h2>{JSON.stringify(albumDetails.artists)}</h2>
-                {getImage(albumDetails)}
-
-
+                {formatTracks(albumDetails)}
             </div>
         </div>
 
--- front-end/src/components/SearchScreen/ArtistDetailsScreen.js
+++ front-end/src/components/SearchScreen/ArtistDetailsScreen.js
@@ -1,4 +1,8 @@
 const ArtistDetailsScreen = () => {
+    return(
+      <h1>Artist Details Page</h1>
+
+    );
 
 };
 export default ArtistDetailsScreen;
