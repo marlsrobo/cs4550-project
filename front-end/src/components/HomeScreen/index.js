e376fbd6d79747e27b4045905618dcd8ae56fb88
Laura Calderon <calderon.l@northeastern.edu>
2022-04-18 14:14:08 -0400

    adding privacy policy to home screen

--- front-end/src/components/HomeScreen/index.js
+++ front-end/src/components/HomeScreen/index.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from "react";
 import NavigationSidebar from "../NavigationSidebar";
 import {Outlet, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import axios from "axios";
+import './index.css';
 
 const api = axios.create({
     withCredentials: true
@@ -38,12 +40,19 @@ const HomeScreen = () => {
             <div className="col-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="home"/>
             </div>
+
             {/*<div className="col-10 col-lg-7 col-xl-6">*/}
             {/*    <Outlet/>*/}
             {/*</div>*/}
             {/*<div className="d-none d-lg-block col-lg-4 col-xl-4">*/}
             {/*    <WhoToFollowList/>*/}
             {/*</div>*/}
+            <div className="pos-fixed dimension-landscape bg-color-dark-gray pt-3 ps-5">
+                <b>Notice.</b> By using our website, you agree to our <Link to="/privacy-policy">Privacy Policy.</Link>
+                {/*<Link to="/privacy-policy">*/}
+                {/*   <button className='btn btn-primary mb-3 me-3 float-right'>Read Privacy Policy</button>*/}
+                {/*</Link>*/}
+            </div>
         </div>
     );
 };
