ee9fb489c4e067a8f33235920b8b1220cd0c86f6
Marley Robinson <marlsrobo@gmail.com>
2022-04-21 14:47:44 -0400

    changing album db

--- back-end/database/albums/albums-schema.js
+++ back-end/database/albums/albums-schema.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 const albumsSchema = mongoose.Schema({
     name: String,
-    artists: String,
     albumId: String,
-    albumCover: String,
     likes: {type: Number, default: 0},
     dislikes: {type: Number, default: 0},
 }, {collection: 'albums'})
