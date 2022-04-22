f5976d5833d519acb14b5c4dca2c908b5f4af5e2
Marley Robinson <marlsrobo@gmail.com>
2022-04-22 12:52:07 -0400

    adding date of review to schema

--- back-end/database/album-reviews/album-reviews-schema.js
+++ back-end/database/album-reviews/album-reviews-schema.js
@@ -3,6 +3,7 @@ const albumReviewsSchema = mongoose.Schema({
     review: String,
     albumId: String,
     reviewerEmail: String,
+    datePosted: Date,
     reviewer: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'UsersModel'}
